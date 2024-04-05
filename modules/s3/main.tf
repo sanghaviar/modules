36f1690f3cd0f66e632d55d81c9ed7cdb6bac1e3
sanghavi <sanghavi.a.r@diggibyte.com>
2024-04-05 18:22:30 +0530

    updated codes

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,3 @@
-
-
 resource "aws_s3_bucket" "root_storage_bucket" {
   bucket = var.config["aws_s3_bucket_name"]
   force_destroy = true
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,2 +1,1 @@
-variable "ACCOUNT_ID" {}
 variable "config" {}
