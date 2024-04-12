8298e9004f54a3089c3cbc5342f52ec53b916e43
sanghavi <sanghavi.a.r@diggibyte.com>
2024-04-12 15:40:36 +0530

    s3 providers

--- aws_res/s3/providers.tf
+++ aws_res/s3/providers.tf
@@ -1,12 +1,12 @@
-#terraform {
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "5.33.0"
-#    }
-#    databricks = {
-#      source  = "databricks/databricks"
-#      version = ">=1.9.2"
-#    }
-#  }
-#}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.33.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = ">=1.9.2"
+    }
+  }
+}
