96c9c5a9d0799c02c888ad488f62d606bbe2fcc9
sanghavi <sanghavi.a.r@diggibyte.com>
2024-04-15 13:59:40 +0530

    res

--- aws_res/iam/main.tf
+++ aws_res/iam/main.tf
@@ -9,9 +9,9 @@ data "databricks_aws_crossaccount_policy" "aws_crossaccount_policy" {
 #  bucket = "cueboxbucket119087"
 #}
 
-data "aws_s3_bucket_policy" "bucket_policy"{
-  bucket = var.config["aws_s3_bucket"]
-}
+#data "aws_s3_bucket_policy" "bucket_policy"{
+#  bucket = var.config["aws_s3_bucket"]
+#}
 
 resource "aws_iam_role" "cross_account_role" {
   name = var.config["aws_iam_role_name"]
@@ -25,12 +25,12 @@ resource "aws_iam_role_policy" "role_policy" {
   role   = aws_iam_role.cross_account_role.id
 
 }
-resource "aws_iam_role_policy" "bucket_policy" {
-  depends_on = [aws_iam_role.cross_account_role]
-  name = var.config["aws_iam_bucket_policy_name"]
-  policy = jsonencode(jsondecode(file("config/${var.config["bucket_policy_filename"]}")))
-  role   = aws_iam_role.cross_account_role.id
-}
+#resource "aws_iam_role_policy" "bucket_policy" {
+#  depends_on = [aws_iam_role.cross_account_role]
+#  name = var.config["aws_iam_bucket_policy_name"]
+#  policy = jsonencode(jsondecode(file("config/${var.config["bucket_policy_filename"]}")))
+#  role   = aws_iam_role.cross_account_role.id
+#}
 
 
 
